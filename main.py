63ba3332109e1d9dc12c78b4b6774ec3eb72a371
sirapol <s.sirapol@hotmail.com>
2020-09-01 13:38:51 +0700

    Test completed

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import json
+import requests
+
+host = "yourThingworxHostName"
+port = 8080         # your port number
+appkey = "yourApplicationKey"
+thingName = "yourThingName"
+
+header = {"appKey": appkey, "Content-Type": "application/json"}
+
+strData = {"humid": 25}     # your data
+
+url = host + ':' + str(port) + "/Thingworx/Things/" + \
+    thingName + "/Properties/*"
+
+print(url)
+
+jsonData = json.dumps(strData)
+
+print(jsonData)
+
+response = requests.put(url, data=jsonData, headers=header)
+print(response)
